6ac2c80292b2585f052b445063b3438240ffedab
yaoyao-1113 <62847648+yaoyao-1113@users.noreply.github.com>
2020-12-09 20:58:06 -0500

    Update ui.R

--- hawaii_dashboard/ui.R
+++ hawaii_dashboard/ui.R
@@ -1,33 +1,44 @@
+## 1. header -------------------------------
+header <- dashboardHeader(title = "Go to Hawaii")
 
-
+## 2. siderbar ------------------------------
 sidebar <- dashboardSidebar(
                             width = 260,
                             br(),
                             sidebarMenu(
+                              ## 1st tab show the Main dashboard ------------
                               menuItem(text = "Overview", 
                                        tabName = "Overview",
                                        icon = icon("compass")
                                        ),
-                              menuItem(text = "Flights", 
-                                       tabName = "Flights",
-                                       icon = icon("plane")
-                                      ),
+                              
+                              ## 2nd tab show the Flight info ---------------
+                              menuItem(text = "Flight", 
+                                       tabName = "Flight",
+                                       icon = icon("fighter-jet")
+                              ),
+        
+                              ## 3rd tab shows Airbnb info ------------------
                               menuItem(text = "Airbnb", 
                                        tabName = "Airbnb",
-                                       icon = icon("hotel")
-                                      ),
+                                       icon = icon("house-user")
+                              ),
+                              
+                              ##4th tab shows Attraction info ---------------
                               menuItem(text = "Attraction", 
                                        tabName = "Attraction",
                                        icon = icon("heart")
-                                      )
-                                  )
+                              )
+                              
+                            )
                             
 )
 
 
-# Dashboard Body ----------------------------------------------------------
+## 3. body ----------------------------------------------
 body <- dashboardBody(
   tabItems(
+    ## 3.1 Main dashboard -------------------------------
     tabItem(
       tabName = "Overview",
       tabsetPanel(
@@ -42,7 +53,7 @@ body <- dashboardBody(
               valueBoxOutput("progressBox3", width = 5),
               valueBoxOutput("progressBox4", width = 5)
             ),
-
+            
             sidebarPanel(
               h4("Please select which County you would like to see?", align = "center"),
               position = "right",
@@ -52,9 +63,7 @@ body <- dashboardBody(
                 label = h3("Select box"),
                 choices = mylist
               )
-              
             )
-            
           ),
           fluidRow(
             gradientBox(
@@ -77,15 +86,166 @@ body <- dashboardBody(
           )
         )
       )
+    ),
+    
+    ## 3.2 Flight -------------------------------------------------
+    tabItem(
+      tabName = "Flight",
+      tabsetPanel(
+        type = "tabs",
+        tabPanel(
+          title = "Flight to Hawaii",
+          fluidRow(
+            column (5, 
+              selectInput(
+                "", #need to update to top 20
+                label = h3("From:"),
+                choices = mylist #need to update to top 20 city !!!!!!!!!!!!
+                )
+            ),
+            # column(5,
+            #   selectInput(
+            #     "county_filter",
+            #     label = h3("To:"),
+            #     choices = mylist
+            #     )
+            # )
+          ),
+          hr(),
+          fluidRow(
+            gradientBox(
+              title = "Non-stop Flights",
+              icon = "helicopter",
+              gradientColor = "green", 
+              boxToolSize = "s", 
+              footer = "", #need to update - return the result of flight query!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+              width = 12
+            )
+          )
+        )
+      )
+    ),
+    ## 3.3 Airbnb info ------------------------------------------------------
+    tabItem(
+      tabName = "Airbnb",
+      tabsetPanel(
+        type = "tabs",
+        tabPanel(
+          title = "Place to Stay",
+          # fluidRow(
+          #   column(4,
+          #          selectInput(
+          #            "county_filter",
+          #            label = h3("County to Visit"),
+          #            choices = mylist  #need to update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          #          )
+          #   ),
+          fluidRow(
+            column (8,
+                    sliderInput(inputId = "price_range",
+                                label = "Price Range",
+                                min = 0,
+                                max = 1000,
+                                value = c(100,500),
+                                width = "220px")
+            ),
+            column (8,
+                    sliderInput(inputId = "night",
+                                label = "Nights",
+                                min = 1,
+                                max = 30,
+                                value = 10,
+                                width = "220px")
+            )
+          ),
+          hr(),
+          fluidRow(
+            column(4,
+                   selectInput(
+                     "county_filter",
+                     label = h3("Bedroom"),
+                     choices = mylist  #need to update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                   )
+          ),
+            column(4,
+                 selectInput(
+                   "county_filter",
+                   label = h3("Bathroom"),
+                   choices = mylist   #need to update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                 )
+          )
+        ),
+        
+        fluidRow(
+          column(4,
+                 selectInput(
+                   "county_filter",
+                   label = h3("Type"),
+                   choices = mylist  #need to update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                 )
+          ),
+          column(4,
+                 checkboxInput("superhose",label = "Superhost"))
+        ),
+        hr(),
+        fluidRow(
+          gradientBox(
+            title = "List of recommandation",
+            icon = "fa fa-heart",
+            gradientColor = "red",
+            boxToolSize = "s",
+            footer = "", #need to update - return the result of flight query!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            width = 12
+          )
+        )
+      )
+    )
+  ),
+  ## 3.4 Attraction info ------------------------------------------------------
+  tabItem(
+    tabName = "Attraction",
+    tabsetPanel(
+      type = "tabs",
+      tabPanel(
+        title = "Place to see",
+        fluidRow(
+          column(4,
+                 selectInput(
+                   "county_filter",
+                   label = h3("Neighborhood"),
+                   choices = mylist  #need to update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                 )
+          ),
+          column(4,
+                 selectInput(
+                   "county_filter",
+                   label = h3("Category"),
+                   choices = mylist   #need to update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                 )
+          )
+        ),
+        hr(),
+        fluidRow(
+          gradientBox(
+            title = "List of Attraction",
+            icon = "camera-retro",
+            gradientColor = "yellow",
+            boxToolSize = "s",
+            footer = "", #need to update - return the result of flight query!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            width = 12
+          )
+        )
+      )
     )
   )
+  )
 )
-        
 
+## 4. put UI together ----------------------------------------------
 
 ui <- dashboardPage(
-  includeCSS("styles.css"),
-  header = dashboardHeader(title = "Go to Hawaii"),
-  sidebar = sidebar,
-  body = body
+  #includeCSS("styles.css"), ask JJ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  header,
+  sidebar,
+  body
 )
